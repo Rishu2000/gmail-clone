3fb0e8953e7dcd0712b3aafa65295b5c7747bc64
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-02-27 21:33:35 +0530

    Completed Header Part by mostly using grid.

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,37 @@
 import React from 'react'
 import styled from "styled-components"
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import SettingsIcon from '@material-ui/icons/Settings';
+import AppsIcon from '@material-ui/icons/Apps';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 function Header() {
     return (
         <Wrapper>
-            <h1>Ris</h1>
+            <LogoWrapper>
+                <Menu>
+                    <MenuIcon/>
+                </Menu>
+                <Logo>
+                <img src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_2x_r2.png" alt=""/>
+                </Logo>
+            </LogoWrapper>
+            <SearchWrapper>
+                <SearchBarWrapper>
+                    <SearchIconBar/>
+                    <input type="text" placeholder="Search mail"/>
+                    <ExpandMoreIcon/>
+                </SearchBarWrapper>
+            </SearchWrapper>
+            <IconsWrapper>
+                <HelpOutlineIcon/>
+                <SettingsIcon/>
+                <AppsIcon/>
+                <AccountCircleIcon/>
+            </IconsWrapper>
         </Wrapper>
     )
 }
@@ -12,5 +39,64 @@ function Header() {
 export default Header
 
 const Wrapper = styled.div`
-    background-color:orange;
+    display:grid;
+    grid-template-columns:270px auto 170px;
+    border-bottom:1px solid lightgray;
+    height:70px;
+    align-items:center;
+`
+
+const LogoWrapper = styled.div`
+    height:45px;
+    display:grid;
+    grid-template-columns:25% auto;
+`
+
+const Menu = styled.div`
+    display:grid;
+    place-items:center;
+`
+
+const SearchWrapper = styled.div``
+
+const SearchBarWrapper = styled.div`
+    background:#f1f3f4;
+    width:100%;
+    max-width:750px;
+    display:grid;
+    grid-template-columns:10% auto 7%;
+    place-items:center;
+    height:45px;
+    border-radius:6px;
+
+    input{
+        width:100%;
+        height:30px;
+        background:none;
+        border:none;
+        font-size:18px;
+        
+        :focus{
+            outline:none;
+        }
+    }
+`
+
+const SearchIconBar = styled(SearchIcon)`
+    color:#5f6368;
+`
+
+const IconsWrapper = styled.div`
+    display:grid;
+    grid-template-columns:repeat(4,auto);
+    margin-left:8px;
+
+    .MuiSvgIcon-root{
+        color:#5f6368;
+    }
+`
+
+const Logo = styled.div`
+    display:flex;
+    height:45px;
 `
